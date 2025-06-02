50dff00c3e07c3d384966ca78ddf0fdd6d58ceca
NguyenNgoAnhhTu <anhtu113kx@gmail.com>
2025-06-02 23:56:22 +0700

    feat(qr): add qrcode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,7 +62,16 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation("androidx.compose.material:material-icons-extended:1.6.1")
 
+    implementation("com.google.zxing:core:3.5.2")
+    implementation("com.journeyapps:zxing-android-embedded:4.3.0")
 
+    implementation("androidx.camera:camera-core:1.3.1")
+    implementation("androidx.camera:camera-camera2:1.3.1")
+    implementation("androidx.camera:camera-lifecycle:1.3.1")
+    implementation("androidx.camera:camera-view:1.3.1")
+
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.google.accompanist:accompanist-permissions:0.32.0")
     implementation(libs.hilt.android)
     //kapt(libs.hilt.android.compiler)
     ksp(libs.hilt.android.compiler)
