ca0a3584f946845f8bead74c564b13bc9cbeee83
NguyenNgoAnhhTu <anhtu113kx@gmail.com>
2025-06-06 00:55:10 +0700

    fix(kts): fix build.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,7 @@
+import org.gradle.kotlin.dsl.android
+import org.gradle.kotlin.dsl.dependencies
+import org.jetbrains.kotlin.commonizer.OptimisticNumberCommonizationEnabledKey.alias
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
@@ -9,12 +13,12 @@ plugins {
 
 android {
     namespace = "org.com.hcmurs"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "org.com.hcmurs"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -22,8 +26,8 @@ android {
         vectorDrawables {
             useSupportLibrary = true
         }
-        manifestPlaceholders["appAuthRedirectScheme"] = "org.com.hcmurs"
 
+        manifestPlaceholders["appAuthRedirectScheme"] = "org.com.hcmurs"
     }
 
     buildTypes {
@@ -53,29 +57,55 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation("androidx.compose.material:material-icons-extended:1.6.1")
 
+    implementation(libs.androidx.material.icons.extended)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.appcompat)
 
-    implementation(libs.hilt.android)
-    //kapt(libs.hilt.android.compiler)
     ksp(libs.hilt.android.compiler)
     ksp(libs.androidx.room.compiler)
 
+    implementation(libs.hilt.android)
+    implementation(libs.androidx.hilt.navigation.compose)
+
+    implementation(libs.composeIcons.fontAwesome)
+    implementation("com.google.zxing:core:3.5.2")
+    implementation("com.journeyapps:zxing-android-embedded:4.3.0")
+
+    implementation("androidx.camera:camera-core:1.3.1")
+    implementation("androidx.camera:camera-camera2:1.3.1")
+    implementation("androidx.camera:camera-lifecycle:1.3.1")
+    implementation("androidx.camera:camera-view:1.3.1")
+
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.google.accompanist:accompanist-permissions:0.32.0")
+
+    implementation(libs.okhttp)
+
+    implementation(libs.gson)
+    implementation (libs.retrofit)
+    implementation (libs.converter.gson)
+
     implementation(libs.accompanist.pager)
+    implementation(libs.androidx.core.splashscreen)
+    implementation(libs.coil.compose)
+
+    implementation(libs.coil.network.okhttp)
+
     implementation(libs.osmdroid.android)
 
     implementation(libs.appauth)
-    implementation(libs.androidx.hilt.navigation.compose)
-    implementation(libs.androidx.animation)//androidx.compose.animation:animation:1.6.7
+    implementation(libs.androidx.browser)
+    implementation(libs.androidx.animation)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
@@ -84,6 +114,9 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+
+
 }
 
 //kapt {
